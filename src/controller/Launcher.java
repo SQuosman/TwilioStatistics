313b25c0f30418d0aae851e2a061543b0354a921
Stefan Quosbarth <usedtobegmx@gmail.com>
2016-05-24 17:53:59 +0200

    0.5
    included executable jar

--- src/controller/Launcher.java
+++ src/controller/Launcher.java
@@ -3,11 +3,8 @@ package controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import model.TwilioStatConnector;
-import view.Gui;
 
 public class Launcher extends Application{
-	
-	private TwilioStatConnector connector;
 
 	// creates the model and launches the controller
 	public static void main(String[] args) {
